3daa20d2c6b1b1f5d8fa0620de1727d54f9600b4
JosDavP <jose.pallares@epn.edu.ec>
2024-01-28 00:10:55 -0500

    Idiomas

--- Exobot.java
+++ Exobot.java
@@ -1,12 +1,22 @@
-public class Exobot extends IABOT implements IEspanol,IIngles{
+public class Exobot extends IABot implements IEspanol,IIngles{
     private Botas botas;
     private Reactor reactor;
     private Laser laser;
     private Metralleta metralleta;
     private Lanzafuegos lanzafuego;
     private Bazuca bazuca;
+    private Exobot exobot;
+
+    public Exobot getExobot() {
+        return exobot;
+    }
+
+    public void setExobot(Exobot exobot) {
+        this.exobot = exobot;
+    }
 
     public Exobot(){
+        this.exobot= new Exobot();
         this.botas = new Botas(true);
         this.reactor = new Reactor(true);
         this.equiparlaser(false);
@@ -30,5 +40,12 @@ public class Exobot extends IABOT implements IEspanol,IIngles{
     public void equiparlanzafuegos(boolean equipar){
         this.lanzafuego = new Lanzafuegos(equipar);
     }
-
+    @Override
+    public String pjenseniarIngles() {
+        return "El "+this.getExobot()+"ha aprendido español.";
+    }
+    @Override
+    public String pjensenaEspanol() {
+        return "El "+this.getExobot()+"ha aprendido español.";
+    }
 }
--- IEspanol.java
+++ IEspanol.java
@@ -1,3 +1,4 @@
 public interface IEspanol {
+    public String pjensenaEspanol();
 
 }
