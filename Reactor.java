52e4ef2350f7d7f8a61865a3f53693f96ed80603
Johann Pasquel <133042421+Vladimirjon@users.noreply.github.com>
2024-01-28 17:24:56 -0500

    Update Reactor.java

--- Reactor.java
+++ Reactor.java
@@ -1,15 +1,19 @@
 public class Reactor {
-    private boolean pjreactor=true;
+    private boolean pjreactor;
+
+    public boolean isPjreactor() {
+        return pjreactor;
+    }
+
+    public void setPjreactor(boolean pjreactor) {
+        this.pjreactor = pjreactor;
+    }
 
     public boolean ispjReactorequipped() {
         return pjreactor;
     }
 
-    public Reactor(boolean pjreactor){
-        this.pjreactor = pjreactor;
-    }
-
-    public void pjVolar(){
-        System.out.println("Volando mientras haya energia");
-    }
+    // public Reactor(boolean pjreactor){
+    //     this.pjreactor = pjreactor;
+    // }
 }
