9b78345048dc446203d49a8eb5345d3904ffc2a9
Johann Pasquel <133042421+Vladimirjon@users.noreply.github.com>
2024-01-28 17:21:42 -0500

    Update Lanzafuegos.java

--- Lanzafuegos.java
+++ Lanzafuegos.java
@@ -10,7 +10,15 @@ public class Lanzafuegos implements IArma{
         System.out.println("Utilizando lanzafuegos");
     }
 
-    public Lanzafuegos(boolean pjlanzafuego){
+    public Lanzafuegos(Exobot oExobot){
+         this.pjlanzafuego = oExobot.getPjequiparlanzafuego();
+     }
+
+    public boolean isPjlanzafuego() {
+        return pjlanzafuego;
+    }
+
+    public void setPjlanzafuego(boolean pjlanzafuego) {
         this.pjlanzafuego = pjlanzafuego;
     }
 
