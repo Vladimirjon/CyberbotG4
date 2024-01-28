88128dcfeec01276a32761cffc5bdb10ec2195a9
Johann Pasquel <133042421+Vladimirjon@users.noreply.github.com>
2024-01-28 17:56:36 -0500

    Update Mecatronico de Jose

--- Mecatronico.java
+++ Mecatronico.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 
 public class Mecatronico {
+    private static int VAcontadorExobots = 0;
     //Esta el constructor donde se crean todas las variables aleatoriamente
     public Exobot oExobot;
     public Lanzafuegos oLanzafuegos;
@@ -17,6 +18,18 @@ public class Mecatronico {
     }
    
     public static void main(String[] args) {
+        Mecatronico mecatronico = new Mecatronico();
+        Exobot[] exobots = new Exobot[20];
+
+        for (int i = 0; i < 20; i++) {
+            exobots[i] = new Exobot(false, false, false, false, false, false);
+            String serieAleatoria = mecatronico.vaAsignarSerie();
+            exobots[i].vaAsignarSerie(serieAleatoria);
+        }
+
+        for (Exobot exobot : exobots) {
+            exobot.vaMostrarInformacion();
+        }
         ArrayList<Mecatronico> TJomecatronico = new ArrayList<>();
         
         for(int i = 0; i<20; i++){
@@ -28,6 +41,18 @@ public class Mecatronico {
             // Hacer algo con exobotActual
         }
         
+    }
+    public String vaAsignarSerie() {
+        StringBuilder VAserie = new StringBuilder();
+        String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        Random random = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            int indice = random.nextInt(caracteres.length());
+            VAserie.append(caracteres.charAt(indice));
+        }
+
+        return VAserie.toString();
     }
   
 
