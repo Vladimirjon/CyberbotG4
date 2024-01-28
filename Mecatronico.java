ec99562d68a0f4e9930c5969637cf92fb5f587d6
Johann Pasquel <133042421+Vladimirjon@users.noreply.github.com>
2024-01-28 17:23:24 -0500

    Update Mecatronico.java

--- Mecatronico.java
+++ Mecatronico.java
@@ -1,5 +1,34 @@
+import java.util.ArrayList;
+
 public class Mecatronico {
+    //Esta el constructor donde se crean todas las variables aleatoriamente
+    public Exobot oExobot;
+    public Lanzafuegos oLanzafuegos;
+    public Metralleta oMetralleta;
+    public Laser oLaser;
+    public Bazuca oBazuca;
+
+    public Mecatronico(){
+        oExobot = new Exobot();
+        oLanzafuegos =new Lanzafuegos(oExobot);
+        oMetralleta = new Metralleta(oExobot);
+        oLaser = new Laser(oExobot);
+        oBazuca = new Bazuca(oExobot);
+    }
+   
     public static void main(String[] args) {
+        ArrayList<Mecatronico> TJomecatronico = new ArrayList<>();
+        
+        for(int i = 0; i<20; i++){
+            TJomecatronico.add(new Mecatronico());
+        }
+
+        for (int i = 0; i < TJomecatronico.size(); i++) {
+            System.out.println(TJomecatronico.get(i).oLanzafuegos.ispjLanzafuegoequipped());
+            // Hacer algo con exobotActual
+        }
         
     }
+  
+
 }
